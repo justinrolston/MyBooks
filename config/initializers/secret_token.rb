81b8b135568ed7c4f6cf27c4ea6e415cf3ecd3e7
Justin Rolston <justinrolston@gmail.com>
2013-06-04 22:00:42 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBooks::Application.config.secret_token = 'b539df3681cccaaf1fc5926fb0852a4e8c7953aa0328add3b3c417b080191678b9aba9d0348ec636c62222ff7b65789d23a591722451ef7e4fb87790f5a57c42'
