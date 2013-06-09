032f84fd677331a15b8b719cce7cc00996f9a76c
Justin Rolston <justinrolston@gmail.com>
2013-06-09 16:59:45 -0400

    added author

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130605024836) do
+ActiveRecord::Schema.define(:version => 20130609205810) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -46,12 +46,17 @@ ActiveRecord::Schema.define(:version => 20130605024836) do
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
+  create_table "authors", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "books", :force => true do |t|
     t.string   "title"
     t.string   "title_long"
     t.string   "isbn10"
     t.string   "isbn13"
-    t.string   "author"
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
