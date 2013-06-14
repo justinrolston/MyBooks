d2ec0c8bd09bfdc1581148a97a5aea03a08eb2b6
Justin Rolston <justinrolston@gmail.com>
2013-06-13 21:42:37 -0400

    added author build

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -1,11 +1,27 @@
 require 'openlibrary'
 
 ActiveAdmin.register Book do
+  form do |f|
+    f.inputs do
+      if f.object.new_record?
+        f.input :isbn
+      else
+        f.input :author,  :as => :select,      :collection => Author.all
+        f.input :title
+        f.input :subtitle
+        f.input :isbn10
+        f.input :isbn13
+        f.input :description
+      end
+      f.actions
+    end
+  end
+
   controller do
     def create
-      @book = Book.new(params[:book])
+      @book = Book.new()
       data = Openlibrary::Data
-      book_details = data.find_by_isbn(@book.isbn10)
+      book_details = data.find_by_isbn(params[:book][:isbn])
       if book_details.nil?
         respond_to do |f|
           f.html { flash[:alert] = 'Cannot find book'
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,13 +1,14 @@
 class Book < ActiveRecord::Base
   attr_accessible :description, :isbn10, :isbn13, :title, :subtitle, :author_id
+  attr_accessor :isbn
   belongs_to :author
+  accepts_nested_attributes_for :author
 
   self.per_page = 5
 
   def import_openlibrary_data( data)
-    binding.pry
     self.title = data.title unless data.title.nil?
-    self.author = data.authors.first['name'] unless data.authors.nil?
+    self.build_author(:name =>  data.authors.first['name']) unless data.authors.nil?
     self.subtitle = data.subtitle
     self.isbn10 = data.identifiers['isbn_10'].first unless data.identifiers['isbn_10'].nil?
     self.isbn13 = data.identifiers['isbn_13'].first unless data.identifiers['isbn_13'].nil?
