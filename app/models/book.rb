3b6db7c815ae831d72c64b3e72bd6144e216949b
Justin Rolston <justinrolston@gmail.com>
2013-06-14 21:29:17 -0400

    added author logic

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,7 +8,16 @@ class Book < ActiveRecord::Base
 
   def import_openlibrary_data( data)
     self.title = data.title unless data.title.nil?
-    self.build_author(:name =>  data.authors.first['name']) unless data.authors.nil?
+    unless data.authors.nil?
+      @name = data.authors.first['name']
+      @author = Author.find_by_name(@name)
+      binding.pry
+      if @author.nil?
+        self.build_author(:name => @name )
+      else
+        self.author = @author
+      end
+    end
     self.subtitle = data.subtitle
     self.isbn10 = data.identifiers['isbn_10'].first unless data.identifiers['isbn_10'].nil?
     self.isbn13 = data.identifiers['isbn_13'].first unless data.identifiers['isbn_13'].nil?
