9891011a446f52bfcec73261ae74e840777630d4
Justin Rolston <justinrolston@gmail.com>
2013-06-20 21:19:03 -0400

    add test for no book found

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Book do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Import Openlibrary Data' do
+    context 'No OpenLibrary Data' do
+      it 'book should have no data set' do
+        @book = Book.new
+        @author = Author.new(:name => 'N/A').save
+        @book.import_openlibrary_data(nil)
+        @book.author.name.should =="N/A"
+      end
+    end
+  end
 end
